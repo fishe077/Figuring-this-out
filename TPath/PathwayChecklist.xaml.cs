4bd2d31f3375b13adbc8ee950f6f01baecc45e66
fishe077 <fishe077@cougars.csusm.edu>
2018-11-12 20:22:02 -0800

    Version 1.0

---
+++ TPath/PathwayChecklist.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TPath
+{
+    public partial class PathwayChecklist : TabbedPage
+    {
+        public PathwayChecklist()
+        {
+            InitializeComponent();
+        }
+    }
+}
